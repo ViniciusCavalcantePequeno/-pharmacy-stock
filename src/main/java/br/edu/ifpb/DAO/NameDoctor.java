f6063933537b8da411021a3f4b3c064cf10f4ca4
ViniciusCavalcantePequeno <144026431+ViniciusCavalcantePequeno@users.noreply.github.com>
2024-02-26 11:55:36 -0300

    exceptions

---
+++ src/main/java/br/edu/ifpb/DAO/NameDoctor.java
@@ -0,0 +1,11 @@
+public class NameDoctor {
+    private String nameDoctor;
+}
+ public NameDoctor validateDoctorName(String nameDoctor) {
+        if (NameDoctor nameDoctor.length() < 5 && nameDoctor.length() > 50) {
+            throw new 
+        }
+        if (NameDoctor nameDoctor.length() > 5 && nameDoctor.length() < 50){
+            nameDoctor = this.nameDoctor;
+        }
+    }
---
+++ src/main/java/br/edu/ifpb/DAO/NamePatient.java
@@ -0,0 +1,9 @@
+public class NamePatient {
+    private String namePatient;
+}
+ public NamePatient validatePatientName(String namePatient) {
+        if (NamePatient namePatient.length() < 5 && st.length() > 50) {
+            
+        }
+        return null;
+    }
