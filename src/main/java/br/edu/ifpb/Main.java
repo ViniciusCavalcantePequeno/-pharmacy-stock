8da7384e9c119fc090aba8545ace3501d8e74396
Elderes <abraoborges3@gmail.com>
2024-03-18 11:33:08 -0300

    commit de abraao

--- src/main/java/br/edu/ifpb/Main.java
+++ src/main/java/br/edu/ifpb/Main.java
@@ -1,20 +1,19 @@
 import DAO.FichaDAO;
 import DAO.StockDAO;
-import Objects.Remedios;
+import Objects.Ficha;
+import Objects.Remedio;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Remedios remedio = new Remedios(2, "Nimezulida", 10);
-        // Remedios remedio2 = new Remedios(3, "Dipirona", 10);
-        // StockDAO estoque = new StockDAO();
-        // estoque.adicionarItem(remedio);
-        // estoque.adicionarItem(remedio2);
-        // System.out.println(estoque.listarRemedios());
-        // System.out.println(estoque.buscarRemedioPorId(3));
-        // estoque.removerItem(3);
-        // System.out.println(estoque.listarRemedios());
 
         FichaDAO ficha = new FichaDAO();
+
+        Remedio soro = new Remedio(0, "soro", 25);
         
+        Ficha ficha1 = new Ficha("Ramon de Oliveira Bento", 2424);
+
+        ficha1.setReceita(soro);
+
+        System.out.println(ficha1);
     }
 }
