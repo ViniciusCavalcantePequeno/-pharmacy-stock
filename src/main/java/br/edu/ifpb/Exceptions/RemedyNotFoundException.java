9e50351ba1d2fb31fbbfa791ede2895fcc6acb29
ViniciusCavalcantePequeno <144026431+ViniciusCavalcantePequeno@users.noreply.github.com>
2024-03-18 10:22:00 -0300

    criações de exceptions 2

---
+++ src/main/java/br/edu/ifpb/Exceptions/RemedyNotFoundException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class RemedyNotFoundException extends RuntimeException {
+    public RemedyNotFoundException() {
+        super("Esse remedio não está cadastrado.");
+    }
+}
