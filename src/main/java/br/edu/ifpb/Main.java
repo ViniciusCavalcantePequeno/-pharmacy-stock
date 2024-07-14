fbbed36d2c8bf3cfc39292f53f25295688e535c7
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-07-14 18:53:07 -0300

    Adicionei uma lista de coisas para fazer. Fiz algumas alterações de nomes de objetos e classes. Mudei um pouco da classe main para testes.

--- src/main/java/br/edu/ifpb/Main.java
+++ src/main/java/br/edu/ifpb/Main.java
@@ -7,7 +7,7 @@ import Objects.Remedy;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        PrescriptionDAO ficha = new PrescriptionDAO();
+        PrescriptionDAO bancoDeFichas = new PrescriptionDAO();
         MedicoDAO medico = new MedicoDAO();
         PharmaceuticalDAO farma = new PharmaceuticalDAO();
 
@@ -19,9 +19,7 @@ public class Main {
         ficha1.setReceita(soro);
         ficha1.setReceita(dipirona);
 
-        System.out.println(ficha1);
-
-        ficha.addPrescription(ficha1);
-        System.out.println(ficha.listPrescriptions());
+        bancoDeFichas.addPrescription(ficha1);
+        System.out.println(bancoDeFichas.listPrescriptions());
     }
 }
